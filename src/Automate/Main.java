5bf478e40b1ee4988554501b0cb808f5db2cc5e0
Quent <quentin.vincent@efrei.net>
2021-04-04 19:13:08 +0200

    automate contains method

--- src/Automate/Main.java
+++ src/Automate/Main.java
@@ -8,11 +8,19 @@ public class Main {
 
         Etat etat4 = new Etat("4", null, null, 0, false, true);
         Etat etat3 = new Etat("3", new Etat[]{etat4}, new char[]{'b'}, 1, false, false);
-        Etat etat2 = new Etat("2", new Etat[]{etat4}, new char[]{'a'}, 1, false, false);
+        Etat etat2 = new Etat("2", null, new char[]{'a', MOT_VIDE}, 2, false, false);
         Etat etat1 = new Etat("1", new Etat[]{etat2, etat3}, new char[]{'a', 'b'}, 2, true, false);
+        etat2.transitions = new Etat[]{etat2, etat4};
         Automate automate = new Automate("A", new Etat[]{etat1}, 1, 4);
         automate.afficherAutomate();
         System.out.println("//");
-        System.out.println(etat1.contains("bb"));
+        System.out.println(automate.contains("aaaaa")); //true
+        System.out.println(automate.contains("aaaaa*")); //true, avec ou sans * ne change rien
+        System.out.println(automate.contains("")); //false
+        System.out.println(automate.contains("*")); //false, avec ou sans * ne change rien
+        System.out.println(automate.contains("bb")); //true
+        System.out.println(automate.contains("b")); //false
+        System.out.println(automate.contains("bbb")); //false
+        System.out.println(automate.isAsynchrone()); //true
     }
 }
