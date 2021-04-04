81227b449b3e0c593b3c6cc283fe77330b1fb830
Quent <quentin.vincent@efrei.net>
2021-04-04 18:13:21 +0200

    upgraded etat class

--- src/Automate/Etat.java
+++ src/Automate/Etat.java
@@ -9,7 +9,8 @@ public class Etat {
     int nbTransitions;          //le nombre de transistions sortantes
     boolean entree;             //true si oui, false sinon
     boolean sortie;             //true si oui, false sinon
-    boolean read;                   //true si déjà lu, false sinon
+    boolean read;               //true si déjà lu, false sinon
+    public final char MOT_VIDE = '*';
 
     Etat(String label, Etat[] transitions, char[] charTransitions, int nbTransitions, boolean entree, boolean sortie){
         this.label = label;
@@ -41,4 +42,56 @@ public class Etat {
         }
         return text;
     }
+
+    void resetRead() {
+        read = false;
+        for (int i = 0; i < nbTransitions; i++){
+            if (transitions[i].read) transitions[i].resetRead();
+        }
+    }
+
+    void printEtat() {
+        System.out.print("\n(");
+        if (entree) System.out.print("->");
+        System.out.print(label);
+        if (sortie) System.out.print("->");
+        System.out.print(")");
+        if (nbTransitions > 0) {
+            for (int i = 0; i < nbTransitions; i++) {
+                System.out.print(" /--" + charTransitions[i] + "->");
+                System.out.print("(");
+                if (transitions[i].entree) System.out.print("->");
+                System.out.print(transitions[i].label);
+                if (transitions[i].sortie) System.out.print("->");
+                System.out.print(")");
+            }
+        }
+        read = true;
+    }
+
+    void printAllEtats() {
+         printEtat();
+         if (transitions != null) {
+             for (Etat etat : transitions) {
+                 if (!etat.read) etat.printAllEtats();
+             }
+         }
+    }
+
+    boolean contains(String string, int index) {
+        if (string == null) return false;
+        if (sortie && string.length() == index) return true;
+        if (string.length() <= index) return false;
+        int i = 0;
+        while(i < nbTransitions && charTransitions[i] != string.charAt(index)) {
+            i++;
+        }
+        if (i == nbTransitions) return false;
+        return transitions[i].contains(string, index+1);
+    }
+
+    boolean contains(String string) {
+        return contains(string, 0);
+    }
+
 }
