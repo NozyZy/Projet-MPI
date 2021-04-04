4aa63ca1a37a3fade9a2c48d446ebaf83956a232
Quent <quentin.vincent@efrei.net>
2021-04-04 18:14:22 +0200

    upgraded etat class

--- src/Automate/Main.java
+++ src/Automate/Main.java
@@ -2,10 +2,17 @@ package Automate;
 
 public class Main {
 
+    static final char MOT_VIDE = '*';
+
     public static void main(String[] args) {
-        Etat etat3 = new Etat("3", null, null, 0, false, true);
-        Etat etat2 = new Etat("2", null, null, 0, true, false);
-        Etat etat1 = new Etat("1", new Etat[]{etat2, etat3}, new char[]{'a', 'b'}, 2, false, true);
-        System.out.println(etat1);
+
+        Etat etat4 = new Etat("4", null, null, 0, false, true);
+        Etat etat3 = new Etat("3", new Etat[]{etat4}, new char[]{'b'}, 1, false, false);
+        Etat etat2 = new Etat("2", new Etat[]{etat4}, new char[]{'a'}, 1, false, false);
+        Etat etat1 = new Etat("1", new Etat[]{etat2, etat3}, new char[]{'a', 'b'}, 2, true, false);
+        Automate automate = new Automate("A", new Etat[]{etat1}, 1, 4);
+        automate.afficherAutomate();
+        System.out.println("//");
+        System.out.println(etat1.contains("bb"));
     }
 }
