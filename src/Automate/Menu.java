b56c730ca00b27ca5df2b29857a854f57ccddee4
Ginyto <agianlucca@gmail.com>
2021-04-15 11:25:02 +0200

    deux lignes pour supprimer la console

--- src/Automate/Menu.java
+++ src/Automate/Menu.java
@@ -18,30 +18,57 @@ public class Menu {
         while (menu0 == 1) {
 
             while (menu1 == 1) {
-                System.out.println("Voici le menu : que voulez vous faire ?" + "\n" + 
-                "d : determinisation"+"\n"+"e : exit");
 
+                System.out.println();
+                automate.afficherAutomate();
+                
+                System.out.println(
+                "\n------------------------Voici le menu : que voulez vous faire ?-----------------------------------\n\n" + 
+                "---------------------------> d : determinisation\n" +
+                "---------------------------> s : standardisation\n" +
+                "---------------------------> e : exit\n");
+
+                System.out.print("Tapez votre choix : ");
                 choix = saisie.next();
 
                 
-                if (choix.equals("e")) {
-                   menu1 = 0;
+                if (choix.equals("e")){
+                    System.out.print("\033[H\033[2J");// Effacement de la console
+                    System.out.flush();// Pour une meilleur visibilité
+                    menu1 = 0;
                 }
 
 
                 if (choix.equals("d")) {//Choix determinisation
                     Automate deter = (Automate) automate.clone();
-                    deter.afficherAutomate();
+                    System.out.print("\033[H\033[2J");//Effacement de la console
+                    System.out.flush();//Pour une meilleur visibilité
                     deter.determinisation();
+                    deter.setLabel("Determinisé");
+                    deter.afficherAutomate();
+                }
+
+                if (choix.equals("s")) {// Choix determinisation
+                    Automate standard = (Automate) automate.clone();
+                    System.out.print("\033[H\033[2J");// Effacement de la console
+                    System.out.flush();// Pour une meilleur visibilité
+                    standard.standardisation("i");
+                    standard.setLabel("Standardisé");
+                    standard.afficherAutomate();
                 }
 
             }
             
             System.out.println("Voulez vous quittez ? tapez o");
+            System.out.print("Tapez votre choix : ");
             choix = saisie.next();
+
             if (choix.equals("o")){
+                System.out.print("\033[H\033[2J");// Effacement de la console
+                System.out.flush();// Pour une meilleur visibilité
                 menu0 = 0;
                 saisie.close();
+                
             }
         }
     }
