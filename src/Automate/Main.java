e7b20a1d673be16f26867c28725212762363e6f3
Ginyto <agianlucca@gmail.com>
2021-04-15 16:11:51 +0200

    beta 2.1

--- src/Automate/Main.java
+++ src/Automate/Main.java
@@ -3,19 +3,19 @@ package Automate;
 public class Main {
     public static void main(String[] args) {
 
-        Multifonctions jarvis = new Multifonctions();
-        jarvis.clearConsole();
-        jarvis.autoSelection();
+        //Multifonctions jarvis = new Multifonctions();
+        //jarvis.clearConsole();
+        //jarvis.autoSelection();
 
-        //Automate automate = Readwrite.readAutomateFile("src/Automate/test_automate.txt");
+        Automate automate = Readwrite.readAutomateFile("index/B_automate.txt");
         //assert automate != null;// Verification que l'automate à bien été remplie (Il me semble que dans readwrite la fonction try permet la meme chose a voir)
-        //automate.afficherAutomate();
+        automate.afficherAutomate();
         //System.out.println("test\n");
         //System.out.println();
         //automate.standardisation("i");
         //automate.setLabel("Standardisé");
-        //automate.determinisation();
-        //automate.setLabel("Deterministe");
+        automate.determinisation();
+        automate.setLabel("Deterministe");
         //automate.afficherAutomate();
         //System.out.println("-----------------------------------> "+automate.getEtatEntree(1).getNom());
         //System.out.println("-----------------------------------> "+automate.mitose(automate.getEtats(0)));
