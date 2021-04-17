a42f0356dbd26c8d4c7f5ebf41ca757830f27fe7
quentin.vincent@efrei.net <WBVKS8m71d&o@sdM>
2021-04-17 13:03:56 +0200

    function contains :white_check_mark:

--- src/Automate/Main.java
+++ src/Automate/Main.java
@@ -1,4 +1,4 @@
-
+package Automate;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,12 +10,15 @@ public class Main {
         Automate automate = Readwrite.readAutomateFile("index/B_automate.txt");
         //assert automate != null;// Verification que l'automate à bien été remplie (Il me semble que dans readwrite la fonction try permet la meme chose a voir)
         automate.afficherAutomate();
+        System.out.println(automate.contains("abaa"));
         //System.out.println("test\n");
         //System.out.println();
         //automate.standardisation("i");
         //automate.setLabel("Standardisé");
         automate.determinisation();
         automate.setLabel("Deterministe");
+        automate.afficherAutomate();
+        System.out.println(automate.contains("abaa"));
         //automate.afficherAutomate();
         //System.out.println("-----------------------------------> "+automate.getEtatEntree(1).getNom());
         //System.out.println("-----------------------------------> "+automate.mitose(automate.getEtats(0)));
